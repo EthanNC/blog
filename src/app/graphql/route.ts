04993c5bed3de54fd271832d383ebd96bc1033ae
EthanNC <ethan.n.cumming@gmail.com>
2023-05-22 14:29:39 -0400

    add pothos

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -1,36 +1,22 @@
-import { createSchema, createYoga } from "graphql-yoga";
+import SchemaBuilder from "@pothos/core";
+import { createYoga } from "graphql-yoga";
 
-const schema = createSchema({
-  typeDefs: /* GraphQL */ `
-    type Query {
-      cart(id: ID!): Cart!
-    }
+const builder = new SchemaBuilder({});
 
-    type Cart {
-      id: ID!
-      totalItems: Int!
-      items: [CartItem]
-    }
-
-    type CartItem {
-      id: ID!
-      name: String!
-    }
-  `,
-  resolvers: {
-    Query: {
-      cart: (_, { id }) => ({
-        id,
-        totalItems: 1,
-        items: [{ id: "item-1", name: "Stickers" }],
-      }),
-    },
-  },
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      args: {
+        name: t.arg.string(),
+      },
+      resolve: (parent, { name }) => `hello, ${name || "World"}`,
+    }),
+  }),
 });
 
 const { handleRequest } = createYoga({
   graphqlEndpoint: "/graphql",
-  schema,
+  schema: builder.toSchema(),
   fetchAPI: {
     Request: Request,
     Response: Response,
